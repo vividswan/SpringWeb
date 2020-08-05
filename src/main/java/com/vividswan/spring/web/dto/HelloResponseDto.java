e6499643e5d6c2875ee23aef9eb6f6047ed82d2d
vividswan <vividswan@naver.com>
2020-08-05 21:29:29 +0900

    Create HelloResponseDto

---
+++ src/main/java/com/vividswan/spring/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.vividswan.spring.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
