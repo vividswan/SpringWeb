2edf615a8368eab97a8981700c0bf52148fc930f
vividswan <vividswan@naver.com>
2020-08-06 23:16:01 +0900

    Update HelloControllerTest.java

--- src/test/java/com/vividswan/spring/web/HelloControllerTest.java
+++ src/test/java/com/vividswan/spring/web/HelloControllerTest.java
@@ -7,10 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -19,11 +19,24 @@ public class HelloControllerTest {
     MockMvc mvc;
 
     @Test
-    public void helloTest() throws Exception{
+    public void helloTest() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDtoTest() throws Exception{
+        String name = "vividswan";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name",name)
+                .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
