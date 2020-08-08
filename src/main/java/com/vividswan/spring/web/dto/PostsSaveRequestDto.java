adac91c10bd98a1d5c928764c54211efb26bdfda
vividswan <vividswan@naver.com>
2020-08-08 22:21:20 +0900

    Create PostsSaveRequestDto.java

---
+++ src/main/java/com/vividswan/spring/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.vividswan.spring.web.dto;
+
+import com.vividswan.spring.web.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .author(author)
+                .author(author)
+                .build();
+    }
+}
