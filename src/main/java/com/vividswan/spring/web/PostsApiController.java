f458e69e0100b6702b02b7cf58972f2e9682c7c1
vividswan <vividswan@naver.com>
2020-08-08 22:20:57 +0900

    Create PostsApiController.java

---
+++ src/main/java/com/vividswan/spring/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.vividswan.spring.web;
+
+
+import com.vividswan.spring.service.PostsService;
+import com.vividswan.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
