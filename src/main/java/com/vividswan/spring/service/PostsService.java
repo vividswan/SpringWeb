0dc25c338c5eac4951d534a8f0e5449087fbf503
vividswan <vividswan@naver.com>
2020-08-08 22:21:37 +0900

    Create PostsService.java

---
+++ src/main/java/com/vividswan/spring/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.vividswan.spring.service;
+
+import com.vividswan.spring.web.domain.posts.PostsRepository;
+import com.vividswan.spring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
