a157c3c9a73160675d2c1726972b35f091c5a5ea
vividswan <vividswan@naver.com>
2020-08-09 17:30:48 +0900

    Update PostsService.java

--- src/main/java/com/vividswan/spring/service/PostsService.java
+++ src/main/java/com/vividswan/spring/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.vividswan.spring.service;
 
+import com.vividswan.spring.web.domain.posts.Posts;
 import com.vividswan.spring.web.domain.posts.PostsRepository;
+import com.vividswan.spring.web.dto.PostsResponseDto;
 import com.vividswan.spring.web.dto.PostsSaveRequestDto;
+import com.vividswan.spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +19,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
