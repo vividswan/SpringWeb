0992e7fd637d34b15454237d980515dbc050d0e2
vividswan <vividswan@naver.com>
2020-08-12 23:30:14 +0900

    Create PostsListResponseDto.java

---
+++ src/main/java/com/vividswan/spring/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.vividswan.spring.web.dto;
+
+import com.vividswan.spring.web.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
