3174e122877739d3d4963995f63ea078a3a9c094
vividswan <vividswan@naver.com>
2020-08-13 00:10:14 +0900

    Update IndexController.java

--- src/main/java/com/vividswan/spring/web/dto/IndexController.java
+++ src/main/java/com/vividswan/spring/web/dto/IndexController.java
@@ -1,18 +1,34 @@
 package com.vividswan.spring.web.dto;
 
+import com.vividswan.spring.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto postsResponseDto = postsService.findById(id);
+        model.addAttribute("post",postsResponseDto);
+
+        return "posts-update";
     }
 }
