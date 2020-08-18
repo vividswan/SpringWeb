70c198cfd79d70bd328911d498ea9d6b86da48de
vividswan <vividswan@naver.com>
2020-08-18 22:09:55 +0900

    Create userRepository.java

---
+++ src/main/java/com/vividswan/spring/web/domain/user/userRepository.java
@@ -0,0 +1,10 @@
+package com.vividswan.spring.web.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface userRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
